da3db596e0af7c441415123f4adfd999d4b81946
Mike7872 <maiantelo34@gmail.com>
2025-08-25 22:20:37 -0400

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+return [
+  'db' => [
+    'host' => 'localhost',
+    'name' => 'queens_db',
+    'user' => 'root',
+    'pass' => 'Admin2025',
+    'charset' => 'utf8mb4'
+  ],
+  'app' => [
+    'base_url' => '/Queens/public',
+    'libreria' => '/Queens/vendor/',
+    'usuario' => '/Queens/app/views/users'
+    
+  ]
+];
